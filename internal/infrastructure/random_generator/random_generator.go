4ee0350a5dfe598940a7c26ac7d8cf476460c657
unknown <timkachern@mail.ru>
2025-11-28 21:43:36 +0300

    feat: add implementation of random generator

--- internal/infrastructure/random_generator/random_generator.go
+++ internal/infrastructure/random_generator/random_generator.go
@@ -1,21 +1,32 @@
 package random_generator
 
-import "math/rand"
+import (
+	"math/rand"
 
-type Random struct {
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/pkg/random"
+)
+
+type RandomGenerator struct {
+}
+
+func NewGenerator() *RandomGenerator {
+	return &RandomGenerator{}
+}
+
+type Randomizer struct {
 	r *rand.Rand
 }
 
-func New(seed int64) *Random {
-	return &Random{
+func (r *RandomGenerator) New(seed int64) random.Random {
+	return &Randomizer{
 		r: rand.New(rand.NewSource(seed)),
 	}
 }
 
-func (s *Random) Float64() float64 {
+func (s *Randomizer) Float64() float64 {
 	return s.r.Float64()
 }
 
-func (s *Random) Intn(n int) int {
+func (s *Randomizer) Intn(n int) int {
 	return s.r.Intn(n)
 }
