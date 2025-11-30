1706cb7d1045635be85f82fde02219d951c51dfc
unknown <timkachern@mail.ru>
2025-11-30 15:40:48 +0300

    feat: add main.go to start app

--- cmd/fractalflame/main.go
+++ cmd/fractalflame/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/application"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/infrastructure/cli"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/infrastructure/random_generator"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/infrastructure/renderers"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/infrastructure/saver"
+)
+
 func main() {
-	// TODO: write your code here
+	randomGen := random_generator.NewGenerator()
+
+	logger := NewLogger()
+
+	saver := saver.NewPngSaver()
+	chooser := renderers.NewChooser(randomGen)
+
+	flameService := application.NewFlameService(saver, chooser, logger)
+
+	app := cli.NewApp(flameService, logger)
+
+	err := app.Run(context.Background(), os.Args)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func NewLogger() *slog.Logger {
+	env := os.Getenv("APP_ENV")
+
+	switch env {
+	case "prod":
+		return slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			}),
+		)
+	default:
+		return slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)
+	}
 }
