59ad748063b6ce29f1085fef13dd413912e59289
unknown <timkachern@mail.ru>
2025-12-01 20:31:00 +0300

    feat: add rotate method for Point struct

--- internal/domain/point.go
+++ internal/domain/point.go
@@ -1,5 +1,7 @@
 package domain
 
+import "math"
+
 // Point представляет точку фрактала
 type Point struct {
 	X float64
@@ -8,4 +10,13 @@ type Point struct {
 
 func NewPoint(x, y float64) Point {
 	return Point{X: x, Y: y}
+}
+
+func Rotate(point Point, theta float64) Point {
+	x := point.X
+	y := point.Y
+	xRotated := x*math.Cos(theta) - y*math.Sin(theta)
+	yRotated := x*math.Sin(theta) + y*math.Cos(theta)
+
+	return Point{X: xRotated, Y: yRotated}
 }
